6944d3d771ffc03393f957db2ba91fbcf8426d00
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2020-05-01 15:05:46 +0200

    Add Disney+ direct link for !random serie

--- Commands/Random Serie.js
+++ Commands/Random Serie.js
@@ -11,7 +11,7 @@ module.exports = {
                 "content-type": "application/json;charset=utf-8"
             }
         };
-    
+
         var request = http.request(options, function (res) {
             var chunks = [];
             res.on("data", function (chunk) {
@@ -21,16 +21,16 @@ module.exports = {
                 var body_TV = Buffer.concat(chunks);
                 var data_TV = body_TV.toString();
                 var data_parse_TV = JSON.parse(data_TV);
-    
+
                 TMDB_TV_random = Math.floor(Math.random() * data_parse_TV.total_results);
-    
+
                 if (TMDB_TV_random === 0) {
                     TMDB_TV_page = 1
                 } else {
                     TMDB_TV_page = (Math.trunc(TMDB_TV_random/20))+1
                     TMDB_TV_random = TMDB_TV_random-(20*(TMDB_TV_page-1))
                 }
-    
+
                     var options = {
                         "method": "GET",
                         "hostname": "api.themoviedb.org",
@@ -40,7 +40,7 @@ module.exports = {
                             "content-type": "application/json;charset=utf-8"
                         }
                     };
-    
+
                     var request = http.request(options, function (res) {
                         var chunks = [];
                         res.on("data", function (chunk) {
@@ -50,7 +50,9 @@ module.exports = {
                             var body_TV = Buffer.concat(chunks);
                             var data_TV = body_TV.toString();
                             var data_parse_TV = JSON.parse(data_TV);
-    
+
+                            id_TV = data_parse_TV.results[TMDB_TV_random].media_type+":"+data_parse_TV.results[TMDB_TV_random].id
+
                             var note_TV = ""
                             if (data_parse_TV.results[TMDB_TV_random].vote_average < 0.5) {
                                 note_TV = "Note indisponible"
@@ -62,7 +64,7 @@ module.exports = {
                                     note_TV = note_TV + "🤍"
                                 }
                             }
-    
+
                             var genres_TV = ""
                             for (let i = 0; i < data_parse_TV.results[TMDB_TV_random].genre_ids.length; i++) {
                                 genres_TV = genres_TV + TMDB_genres_TV[data_parse_TV.results[TMDB_TV_random].genre_ids[i]]
@@ -70,11 +72,11 @@ module.exports = {
                                     genres_TV = genres_TV + ", "
                                 }
                             }
-    
+
                             const embed_TV = new Discord.MessageEmbed()
                                 .setColor('#01b4e4')
-                                .setTitle(data_parse_TV.results[TMDB_TV_random].name)
-                                .setURL("https://www.themoviedb.org/"+data_parse_TV.results[TMDB_TV_random].media_type+"/"+data_parse_TV.results[TMDB_TV_random].id)
+                                .setTitle("▶️ "+data_parse_TV.results[TMDB_TV_random].name+" ◀️")
+                                .setURL("https://www.disneyplus.com/"+data_parse_TV.comments[id_TV])
                                 .setDescription(data_parse_TV.results[TMDB_TV_random].overview)
                                 .addFields(
                                     { name: 'Première diffusion', value: data_parse_TV.results[TMDB_TV_random].first_air_date, inline: true },
@@ -84,7 +86,7 @@ module.exports = {
                                 .setThumbnail("https://image.tmdb.org/t/p/original"+data_parse_TV.results[TMDB_TV_random].poster_path)
                                 .setImage("https://image.tmdb.org/t/p/original"+data_parse_TV.results[TMDB_TV_random].backdrop_path)
                                 .setFooter("Discord+ uses the TMDb API but is not endorsed or certified by TMDb.", "https://i.imgur.com/tpO60XS.png");
-                            
+
                             message.channel.send(embed_TV);
                             console.log("Command !random serie use.")
                         });
