bac2bb01ff9190f5e512e9a35ddc6d0c7f4b2623
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2020-05-06 15:34:27 +0200

    Update index.js
    ▶️ Add catch error
    ▶️ Format the document
    ▶️ Add color for console log

--- index.js
+++ index.js
@@ -10,60 +10,60 @@ const TMDB_genres_movie = require("./TMDb/Genre_Movie.json")
 const TMDB_genres_TV = require("./TMDb/Genre_TV.json")
 
 var http = require("https");
- 
+
 const fs = require('fs');
 client.commands = new Discord.Collection();
- 
+
 const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./Commands/${file}`);
     client.commands.set(command.name, command);
 }
 
 client.on("ready", () => {
-    console.log("Bot online !");
-    client.user.setActivity("Disney+ | !info", {type : "WATCHING"})
+    console.log("\x1b[34m%s\x1b[0m", "Bot online !");
+    client.user.setActivity("Disney+ | !info", { type: "WATCHING" })
 });
- 
+
 client.on('message', message => {
     let args = message.content.toLowerCase();
 
     switch (args) {
         case "!info":
             client.commands.get('Info').execute(Discord, message);
-        break;
+            break;
 
         case "!random film":
             client.commands.get('Random Film').execute(TMDB_list_movies, TMDB_api_key, http, TMDB_genres_movie, Discord, message);
-        break;
+            break;
 
         case "!random serie":
             client.commands.get('Random Serie').execute(TMDB_list_TV, TMDB_api_key, http, TMDB_genres_TV, Discord, message);
-        break;
+            break;
 
         case "!random short":
             client.commands.get('Random Short').execute(TMDB_list_shorts, TMDB_api_key, http, TMDB_genres_movie, Discord, message);
-        break;
+            break;
 
         case "!total":
             client.commands.get('Total').execute(TMDB_list_movies, TMDB_api_key, http, TMDB_list_TV, TMDB_list_shorts, message);
-        break;
+            break;
 
         case "!bug":
             client.commands.get('Bug').execute(message)
-        break;
+            break;
 
         case "!credits":
             client.commands.get('Credits').execute(message, package)
-        break;
+            break;
 
         case "!random":
         case "!randomfilm":
-        case "!randomserie":   
+        case "!randomserie":
         case "!randomshort":
             client.commands.get('Old Command').execute(message);
-        break;
+            break;
     }
 });
- 
+
 client.login(process.env.Discord_api_key);
