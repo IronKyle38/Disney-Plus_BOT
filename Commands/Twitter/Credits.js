41c1edc4bf5d8a6dcf2509c177c55f438aa5a1ca
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2020-05-26 18:04:16 +0200

    Add credits command for Twitter

---
+++ Commands/Twitter/Credits.js
@@ -0,0 +1,16 @@
+module.exports = function Credits(Package, Tweet, Twitter_Client) {
+    Reply =
+        "@" + Tweet.user.screen_name +
+        "\nDonnées : @ChroniqueDisney" +
+        "\n" +
+        "\Version : " + Package.version +
+        "\n" +
+        "\nDisney+ BOT uses the TMDb API but is not approved or certified by TMDb, nor affiliated with Disney+." +
+        "\nAll trademarks referenced herein are the properties of their respective owners.";
+
+    console.log("• Command credits use (via Twitter).");
+    Twitter_Client.post('statuses/update', { status: Reply, in_reply_to_status_id: Tweet.id_str })
+        .catch((e) => {
+            console.log(`○ ${e.errors[0].code} : ${e.errors[0].message}`);
+        });
+};
