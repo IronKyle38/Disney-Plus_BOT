3a4917af607c8fa62e21c42d08d2c225964db6f6
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2020-05-26 18:03:59 +0200

    Add bug command for Twitter

---
+++ Commands/Twitter/Bug.js
@@ -0,0 +1,17 @@
+module.exports = function Bug(Tweet, Twitter_Client) {
+    Reply =
+        "Bonjour @" + Tweet.user.screen_name + "," +
+        "\n" +
+        "\nSi tu as découvert un bug, des informations erronées ou si tu souhaites m'envoyer une suggestion n'hésites pas !" +
+        "\nIl te suffit juste de remplir ce formulaire : https://forms.gle/Qo5daMfhW7EUPQck9 ou de m'envoyer un MP." +
+        "\n" +
+        "\nMerci pour ton aide. 💙";
+
+    DM_URL = "https://twitter.com/messages/compose?recipient_id=1264823416978386944";
+
+    console.log("• Command bug use (via Twitter).");
+    Twitter_Client.post('statuses/update', { status: Reply, in_reply_to_status_id: Tweet.id_str, attachment_url: DM_URL })
+        .catch((e) => {
+            console.log(`○ ${e.errors[0].code} : ${e.errors[0].message}`);
+        });
+};
