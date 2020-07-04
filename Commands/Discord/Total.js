cff0d350272de350ef15898320656121b39387c0
IronKyle38 <63471198+IronKyle38@users.noreply.github.com>
2020-07-04 18:00:35 +0200

    Add total of seasons and episodes

--- Commands/Discord/Total.js
+++ Commands/Discord/Total.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'Total',
     description: "Send the total of titles available on Disney+ FR",
-    execute(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV_List_ID, TMDb_Short_List_ID, Discord, message) {
+    execute(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV_List_ID, TMDb_Short_List_ID, TMDb_List, Discord, message) {
         HTTPS.get(`https://api.themoviedb.org/4/list/${TMDb_Movie_List_ID}?api_key=${TMDB_API_Key}`, (req) => {
             let data = '';
 
@@ -32,6 +32,16 @@ module.exports = {
                             req.on('end', () => {
                                 data_parse_short = JSON.parse(data);
 
+                                var Total_Of_Seasons = 0;
+                                for (let i = 0; i < TMDb_List.TV.length; i++) {
+                                    Total_Of_Seasons += TMDb_List.TV[i].Number_Of_Seasons;
+                                };
+
+                                var Total_Of_Episodes = 0;
+                                for (let j = 0; j < TMDb_List.TV.length; j++) {
+                                    Total_Of_Episodes += TMDb_List.TV[j].Number_Of_Episodes;
+                                };
+
                                 Runtime = data_parse_movie.runtime + data_parse_TV.runtime + data_parse_short.runtime;
                                 Days = Math.trunc(Runtime / 1440);
                                 Hours = Math.trunc((Runtime - (Days * 1440)) / 60);
@@ -46,8 +56,10 @@ module.exports = {
                                     .setDescription(`Il y a plus de **${data_parse_movie.total_results + data_parse_TV.total_results + data_parse_short.total_results}** titres différents à visionner sur Disney+ ! 🍿`)
                                     .addFields(
                                         { name: 'Film', value: `**${data_parse_movie.total_results}**`, inline: true },
-                                        { name: 'Série', value: `**${data_parse_TV.total_results}**`, inline: true },
                                         { name: 'Court-métrage', value: `**${data_parse_short.total_results}**`, inline: true },
+                                        { name: 'Série', value: `**${data_parse_TV.total_results}**`, inline: true },
+                                        { name: 'Saisons', value: `**${Total_Of_Seasons}**`, inline: true },
+                                        { name: 'Épisodes', value: `**${Total_Of_Episodes}**`, inline: true },
                                         { name: 'Durée totale', value: `**${Days}** Jours **${Hours}** Heures **${Minutes}** Minutes ⏲️` },
                                         { name: 'Recettes des films', value: `$**${Revenue}** 💸` }
                                     )
--- Commands/Twitter/Total.js
+++ Commands/Twitter/Total.js
@@ -1,4 +1,4 @@
-module.exports = function Total(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV_List_ID, TMDb_Short_List_ID, Twitter_Client, Tweet) {
+module.exports = function Total(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV_List_ID, TMDb_Short_List_ID, TMDb_List, Twitter_Client, Tweet) {
     HTTPS.get(`https://api.themoviedb.org/4/list/${TMDb_Movie_List_ID}?api_key=${TMDB_API_Key}`, (req) => {
         let data = '';
 
@@ -31,6 +31,16 @@ module.exports = function Total(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV
 
                             Total_Titles = data_parse_movie.total_results + data_parse_TV.total_results + data_parse_short.total_results
 
+                            var Total_Of_Seasons = 0;
+                            for (let i = 0; i < TMDb_List.TV.length; i++) {
+                                Total_Of_Seasons += TMDb_List.TV[i].Number_Of_Seasons;
+                            };
+
+                            var Total_Of_Episodes = 0;
+                            for (let j = 0; j < TMDb_List.TV.length; j++) {
+                                Total_Of_Episodes += TMDb_List.TV[j].Number_Of_Episodes;
+                            };
+
                             Runtime = data_parse_movie.runtime + data_parse_TV.runtime + data_parse_short.runtime;
                             Days = Math.trunc(Runtime / 1440);
                             Hours = Math.trunc((Runtime - (Days * 1440)) / 60);
@@ -42,7 +52,8 @@ module.exports = function Total(HTTPS, TMDb_Movie_List_ID, TMDB_API_Key, TMDb_TV
                                 "@" + Tweet.user.screen_name +
                                 "\nIl y a plus de " + Total_Titles + " titres différents à visionner sur Disney+ ! 🍿" +
                                 "\n" +
-                                "\n" + data_parse_movie.total_results + " Films, " + data_parse_TV.total_results + " Séries et " + data_parse_short.total_results + " Court-métrage" +
+                                "\n" + data_parse_movie.total_results + " Films et " + data_parse_short.total_results + " Court-métrage" +
+                                "\n" + data_parse_TV.total_results + " Séries dont " + Total_Of_Seasons + " saisons et " + Total_Of_Episodes + " épisodes." +
                                 "\n" +
                                 "\nDurée totale :" +
                                 "\n" + Days + " Jours " + Hours + " Heures " + Minutes + " Minutes ⏲️" +
